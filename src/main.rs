8e8a7cfb6c328eecd800d3ae6985a6769c58c716
chickencuber <113270822+chickencuber@users.noreply.github.com>
2023-09-13 19:07:34 -0500

    hi

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::env;
+use std::process::Command;
+fn main() {
+    let mut file = env::current_exe().unwrap();
+    file.pop();
+    file.push("Deno Run");
+    file.push("main.ts");
+    let args: Vec<String> = env::args().collect();
+    let deno_script_args: Vec<&str> = args[1..].iter().map(|s| s.as_str()).collect();
+    let mut cmd = Command::new("cmd");
+    cmd.arg("/c").arg("start").arg("cmd").arg("/k").arg("deno").arg("run").arg("--allow-all").arg(file).args(deno_script_args);
+    let _ = cmd.spawn();
+
+}
