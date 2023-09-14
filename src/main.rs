97b42df25d065f8a70a408838f4cba1052cb96fa
chickencuber <113270822+chickencuber@users.noreply.github.com>
2023-09-13 20:58:16 -0500

    hi

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let deno_script_args: Vec<&str> = args[1..].iter().map(|s| s.as_str()).collect();
     let mut cmd = Command::new("cmd");
-    cmd.arg("/c").arg("start").arg("cmd").arg("/k").arg("deno").arg("run").arg("--allow-all").arg("main.ts").args(deno_script_args).current_dir(file);
+    cmd.arg("/c").arg("start").arg("cmd").arg("/k").arg("deno").arg("run").arg("--allow-all").arg("--unstable").arg("main.ts").args(deno_script_args).current_dir(file);
     let _ = cmd.spawn();
 
 }
