8ddd0276233c7ab172d702068cf1ade0d6e966af
chickencuber <113270822+chickencuber@users.noreply.github.com>
2023-09-30 20:03:26 -0500

    hi

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,4 @@ fn main() {
     let mut cmd = Command::new("cmd");
     cmd.arg("/c").arg("start").arg("cmd").arg("/k").arg("deno").arg("run").arg("--allow-all").arg("--unstable").arg("main.ts").args(deno_script_args).current_dir(file);
     let _ = cmd.spawn();
-
 }
