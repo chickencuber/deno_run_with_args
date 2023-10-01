3ec864583f6de2f104198bdd8ba5d23c4a89846d
chickencuber <113270822+chickencuber@users.noreply.github.com>
2023-09-30 20:16:27 -0500

    hi

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ use std::env;
 use std::process::Command;
 fn main() {
     let mut file = env::current_exe().unwrap();
+    let current_dir = env::current_dir().unwrap();
     file.pop();
     file.push("Deno Run");
     let args: Vec<String> = env::args().collect();
     let deno_script_args: Vec<&str> = args[1..].iter().map(|s| s.as_str()).collect();
     let mut cmd = Command::new("cmd");
-    cmd.arg("/c").arg("start").arg("cmd").arg("/c").arg("deno").arg("run").arg("--allow-all").arg("--unstable").arg("main.ts").args(deno_script_args).current_dir(file);
+    cmd.arg("/c").arg("start").arg("cmd").arg("/c").arg("deno").arg("run").arg("--allow-all").arg("--unstable").arg("main.ts").arg(current_dir).args(deno_script_args).current_dir(file);
     let _ = cmd.spawn();
 }
